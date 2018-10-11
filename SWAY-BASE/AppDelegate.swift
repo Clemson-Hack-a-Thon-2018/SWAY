5f472417795b28bb6a1d34f67186f7bc9d151228
JoshuaPaulsen <jpaulsen3110@gmail.com>
2018-10-11 09:09:06 -0400

    morning commit

--- SWAY-BASE/AppDelegate.swift
+++ SWAY-BASE/AppDelegate.swift
@@ -21,6 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let env = "SANDBOX"
     
     
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
