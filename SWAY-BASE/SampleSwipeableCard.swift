99921e3306bb7fa4c2276ab7e70b2357a458cb25
JoshuaPaulsen <jpaulsen3110@gmail.com>
2018-10-16 11:06:46 -0400

    fixed comments

--- SWAY-BASE/SampleSwipeableCard.swift
+++ SWAY-BASE/SampleSwipeableCard.swift
@@ -1,4 +1,8 @@
-//Joshua Paulsen - The Rise Collection
+//  Swipeable-View-Stack
+//
+//  Created by Phill Farrugia on 10/21/17.
+//  Copyright © 2017 Phill Farrugia. All rights reserved.
+//
 
 import UIKit
 import CoreMotion
